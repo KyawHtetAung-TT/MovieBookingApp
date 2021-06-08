b3ee0ee02eaf6fb5934227ddb3caa3346c94cabd
Ryan Willson <93kyawhtetaung@gmail.com>
2021-06-08 21:48:56 +0630

    Movie Time Screen Skeleton Setup

---
+++ MovieBookingApp/MovieBookingApp/View Controller/MovieSeatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieSeatViewController.swift
+//  MovieBookingApp
+//
+//  Created by Ryan Willson on 6/8/21.
+//
+
+import UIKit
+
+class MovieSeatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
