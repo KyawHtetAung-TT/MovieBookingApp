b1ba8d4bdcda06a272ed45e367277079805ce775
Ryan Willson <93kyawhtetaung@gmail.com>
2021-06-24 00:39:09 +0630

    VisaCollectionCell

---
+++ MovieBookingApp/MovieBookingApp/cell/collection view/swifts/VisaCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  VisaCollectionViewCell.swift
+//  MovieBookingApp
+//
+//  Created by Ryan Willson on 6/24/21.
+//
+
+import UIKit
+
+class VisaCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ MovieBookingApp/MovieBookingApp/view controller/VisaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  VisaViewController.swift
+//  MovieBookingApp
+//
+//  Created by Ryan Willson on 6/23/21.
+//
+
+import UIKit
+
+class VisaViewController: UIViewController {
+
+    @IBOutlet weak var visaCollectionCell: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+
+}
